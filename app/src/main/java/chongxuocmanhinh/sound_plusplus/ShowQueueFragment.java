86fe2776a394bf8c2f935c10179a980c63085edc
tinifyphan <tinify.phan@gmail.com>
2016-12-05 21:03:01 +0700

    Update sliding view

--- app/src/main/java/chongxuocmanhinh/sound_plusplus/ShowQueueFragment.java
+++ app/src/main/java/chongxuocmanhinh/sound_plusplus/ShowQueueFragment.java
@@ -1,12 +1,14 @@
 package chongxuocmanhinh.sound_plusplus;
 
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ListView;
 
 /**
  * Created by L on 05/12/2016.
@@ -14,13 +16,21 @@ import android.widget.AdapterView;
 public class ShowQueueFragment extends Fragment
         implements AdapterView.OnItemClickListener
 {
-
+    //Sau này phần này sẽ chuyển sang draggable row nếu có thể
+    private ListView mListView;
     private ShowQueueAdapter mListAdapter;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.showqueue_listview, container, false);
+        Context context = getActivity();
+
+        mListView    = (ListView) view.findViewById(R.id.list);
+        mListAdapter = new ShowQueueAdapter(context, R.layout.draggable_row);
+        mListView.setAdapter(mListAdapter);
+        return view;
     }
 
     //===========================AdapterView.OnItemClickListener=====================//
