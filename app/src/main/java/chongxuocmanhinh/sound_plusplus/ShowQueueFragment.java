c656c0764ce56510d57a62be06735ecee83dab78
tinifyphan <tinify.phan@gmail.com>
2016-12-05 20:29:42 +0700

    Init soundplusplus imagebutton class

--- app/src/main/java/chongxuocmanhinh/sound_plusplus/ShowQueueFragment.java
+++ app/src/main/java/chongxuocmanhinh/sound_plusplus/ShowQueueFragment.java
@@ -1,7 +1,11 @@
 package chongxuocmanhinh.sound_plusplus;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 
 /**
@@ -10,6 +14,15 @@ import android.widget.AdapterView;
 public class ShowQueueFragment extends Fragment
         implements AdapterView.OnItemClickListener
 {
+
+    private ShowQueueAdapter mListAdapter;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
     //===========================AdapterView.OnItemClickListener=====================//
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
---
+++ app/src/main/java/chongxuocmanhinh/sound_plusplus/SoundPlusPlusImageButton.java
@@ -0,0 +1,27 @@
+package chongxuocmanhinh.sound_plusplus;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageButton;
+
+/**
+ * Created by L on 05/12/2016.
+ */
+public class SoundPlusPlusImageButton extends ImageButton {
+
+    public SoundPlusPlusImageButton(Context context) {
+        super(context);
+    }
+
+    public SoundPlusPlusImageButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SoundPlusPlusImageButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+//    public SoundPlusPlusImageButton(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+//        super(context, attrs, defStyleAttr, defStyleRes);
+//    }
+}
