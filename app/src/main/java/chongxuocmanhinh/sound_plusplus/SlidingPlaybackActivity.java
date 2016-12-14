fd7b895c36266ff091cfa3bbeaef3242648d9a4a
tinifyphan <tinify.phan@gmail.com>
2016-12-14 20:48:23 +0700

    Init class SlidingPlaybackActivity for handle playing

---
+++ app/src/main/java/chongxuocmanhinh/sound_plusplus/SlidingPlaybackActivity.java
@@ -0,0 +1,9 @@
+package chongxuocmanhinh.sound_plusplus;
+
+/**
+ * Class này dùng để quản lý phần sliding view của app
+ * có các seekbar và cái button ....
+ * Created by L on 14/12/2016.
+ */
+public class SlidingPlaybackActivity {
+}
