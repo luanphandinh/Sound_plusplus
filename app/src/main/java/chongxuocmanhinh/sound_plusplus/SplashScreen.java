a2cb845285656b7f9e3d96cf4e7dcccd79f9161c
Nang <quynanguit@gmail.com>
2016-12-21 23:28:17 +0700

    modified SplashScreen.java and AndroidMainifest.xml, add animation rotate.xml...... App now able to use SplashScreen

--- app/src/main/java/chongxuocmanhinh/sound_plusplus/SplashScreen.java
+++ app/src/main/java/chongxuocmanhinh/sound_plusplus/SplashScreen.java
@@ -1,10 +1,45 @@
 package chongxuocmanhinh.sound_plusplus;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 /**
  * Created by Nang-PC on 21/12/2016.
  */
 
 public class SplashScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+        final ImageView imgLoading = (ImageView) findViewById(R.id.igmLoading);
+        final Animation an = AnimationUtils.loadAnimation(getBaseContext(),R.anim.rotate);
+        final Animation an2 = AnimationUtils.loadAnimation(getBaseContext(),R.anim.abc_fade_out);
+
+        imgLoading.startAnimation(an);
+        an.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                imgLoading.startAnimation(an2);
+                finish();
+                Intent i = new Intent(getBaseContext(),LibraryActivity.class);
+                startActivity(i);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
 }
