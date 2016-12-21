38e879a0080a70690792eb8fbaf2756d73ea1c1d
Nang <quynanguit@gmail.com>
2016-12-21 22:46:00 +0700

    Init class splashscreen,using for loading app

---
+++ app/src/main/java/chongxuocmanhinh/sound_plusplus/SplashScreen.java
@@ -0,0 +1,10 @@
+package chongxuocmanhinh.sound_plusplus;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Nang-PC on 21/12/2016.
+ */
+
+public class SplashScreen extends AppCompatActivity {
+}
