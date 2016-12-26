51ffd12256e0bf6efa921e6e121185bd70a01eda
tinifyphan <tinify.phan@gmail.com>
2016-12-26 17:01:47 +0700

    Update layout

--- app/src/main/java/chongxuocmanhinh/sound_plusplus/SoundPlusPlusImageButton.java
+++ app/src/main/java/chongxuocmanhinh/sound_plusplus/SoundPlusPlusImageButton.java
@@ -1,7 +1,9 @@
 package chongxuocmanhinh.sound_plusplus;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
+import android.util.TypedValue;
 import android.widget.ImageButton;
 
 /**
@@ -24,15 +26,38 @@ public class SoundPlusPlusImageButton extends ImageButton {
     public SoundPlusPlusImageButton(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         mContext = context;
-
+        mNormalTint = fetchAttrColor(R.attr.controls_normal);
+        mActiveTint = fetchAttrColor(R.attr.controls_active);
+        updateImageTint(-1);
     }
 
     @Override
     public void setImageResource(int resId) {
         super.setImageResource(resId);
+        this.updateImageTint(resId);
     }
 
-    //    public SoundPlusPlusImageButton(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-//        super(context, attrs, defStyleAttr, defStyleRes);
-//    }
+    private void updateImageTint(int resHint) {
+        int filterColor = mNormalTint;
+
+        switch (resHint) {
+            case R.drawable.repeat_active:
+            case R.drawable.repeat_current_active:
+            case R.drawable.stop_current_active:
+            case R.drawable.shuffle_active:
+            case R.drawable.shuffle_album_active:
+            case R.drawable.random_active:
+                filterColor = mActiveTint;
+        }
+
+        this.setColorFilter(filterColor);
+    }
+
+    private int fetchAttrColor(int attr) {
+        TypedValue typedValue = new TypedValue();
+        TypedArray a = mContext.obtainStyledAttributes(typedValue.data, new int[] { attr });
+        int color = a.getColor(0, 0);
+        a.recycle();
+        return color;
+    }
 }
